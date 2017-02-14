6764a1ed0f66331d272279530399e65ff5b28d3d
Azlan Cuttilan <azlan@bynd.com>
2017-02-14 10:53:00 +0000

    Add date picker
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/components/pages/home/AddEvent.vue
+++ src/components/pages/home/AddEvent.vue
@@ -7,19 +7,19 @@
                     <ul>
                         <li>
                             <label for="eventdate">Date:</label>
-                            <input name="eventdate" type="date" v-model="event.date" placeholder="DD/MM/YYYY" />
+                            <datepicker name="eventdate" placeholder="Event date" format="yyyy-MM-dd" v-model="event.date" ></datepicker>
                         </li>
                         <li>
                             <label for="eventvenue">Venue name:</label>
-                            <input name="eventvenue" type="text" placeholder="Venue name" v-model="event.venue" />
+                            <input name="eventvenue" type="text" placeholder="Venue name" v-model="event.venue" required />
                         </li>
                         <li>
                             <label for="eventaddress">Address:</label>
-                            <input name="eventaddress" type="text" placeholder="Venue address" v-model="event.address" />
+                            <input name="eventaddress" type="text" placeholder="Venue address" v-model="event.address" required />
                         </li>
                         <li>
                             <label for="eventimage">Image URL:</label>
-                            <input name="eventimage" type="url" placeholder="Image URL" v-model="event.image" />
+                            <input name="eventimage" type="url" placeholder="Image URL" v-model="event.image" required />
                         </li>
                         <li>
                             <label for="eventwebsite">Website URL:</label>
@@ -38,6 +38,8 @@
     </section>
 </template>
 <script>
+import Datepicker from 'vuejs-datepicker';
+
 export default {
     data() {
         return {
@@ -53,7 +55,6 @@ export default {
             resource: {},
         };
     },
-
     methods: {
         addEvent() {
             const event = {
@@ -78,13 +79,16 @@ export default {
                 });
         },
     },
+    components: {
+        Datepicker,
+    },
     created() {
         this.resource = this.$resource('events.json');
         this.fetchData();
     },
 };
 </script>
-<style scoped>
+<style>
 .add-event {
     box-sizing: border-box;
     width: 100%;
@@ -93,52 +97,60 @@ export default {
     padding: 0 20px 80px;
     clear: both;
     text-align: center;
+
     h2 {
         text-align: center;
         margin: 80px auto 40px;
     }
-}
 
-.container {
-    width: 100%;
-    max-width: 1440px;
-    box-sizing: border-box;
-    margin: 0 auto;
-}
+    .container {
+        width: 100%;
+        max-width: 1440px;
+        box-sizing: border-box;
+        margin: 0 auto;
+    }
 
-fieldset {
-    border: none;
-    padding: 0;
-}
+    fieldset {
+        border: none;
+        padding: 0;
+    }
 
-ul {
-    list-style: none;
-    padding-left: 0;
-    margin: 0;
-}
+    ul {
+        list-style: none;
+        padding-left: 0;
+        margin: 0;
+    }
 
-input {
-    width: 300px;
-    max-width: 100%;
-    @include font(16, 16);
-    padding: 12px 10px 7px;
-    border: 1px solid #2c3e50;
-    box-shadow: inset 0 0 6px 1px rgba(#000, 0.25);
-    appearance: none;
-    margin: 10px 0 40px;
-}
+    input,
+    .datepicker > input {
+        box-sizing: border-box;
+        width: 300px;
+        max-width: 100%;
+        @include font(16, 16);
+        padding: 12px 10px 7px;
+        border: 1px solid #2c3e50;
+        box-shadow: inset 0 0 6px 1px rgba(#000, 0.25);
+        appearance: none;
+        margin: 10px 0 40px;
+    }
 
-label {
-    width: 100%;
-    display: block;
-    @include font(14, 22);
-}
+    label {
+        width: 100%;
+        display: block;
+        @include font(14, 22);
+    }
 
-button {
-    background-color: #eaeaea;
-    border: 1px solid #d5d5d5;
-    border-radius: 5px;
-    padding: 3px 5px;
-    margin-left: 5px;
+    .calendar {
+        left: 50%;
+        margin: 0 0 0 -150px;
+    }
+
+    button {
+        background-color: #eaeaea;
+        border: 1px solid #d5d5d5;
+        border-radius: 5px;
+        padding: 3px 5px;
+        margin-left: 5px;
+    }
 }
 </style>
