4136f327f766d1dd3951262c1e3f01b8a74a9600
Azlan Cuttilan <azlan@bynd.com>
2017-02-15 10:11:46 +0000

    User sign in / out
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/components/pages/home/Home.vue
+++ src/components/pages/home/Home.vue
@@ -10,8 +10,9 @@
                         <button @click.prevent="updateMeister(newMeister)" class="bananameister__submit">Update</button>
                     </form>
                 </transition>
-                <button @click="eventForm = !eventForm" class="bananameister__add-event">{{ eventForm ? 'Cancel' : 'Add Event' }}</button>
-                <button @click="onUserLogin">Sign In</button>
+                <button @click="onUserLogin" class="bananameister__signinout" v-if="!user">Sign In</button>
+                <button @click="onUserSignOut" class="bananameister__signinout" v-if="user">Sign Out</button>
+                <button @click="eventForm = !eventForm" class="bananameister__add-event" v-if="user">{{ eventForm ? 'Cancel' : 'Add Event' }}</button>
             </div>
         </section>
         <!--     <transition name="slide"> -->
@@ -27,11 +28,11 @@ import db from '../../../data/firebase';
 import AddEvent from './AddEvent';
 import ActiveEvent from './ActiveEvent';
 import EventsList from './EventsList';
-import { getUserStatus } from '../../../data/auth';
+// import { getUserStatus } from '../../../data/auth';
 
-const user = getUserStatus;
+const currentuser = firebase.auth().currentuser;
 // eslint-disable-next-line
-console.log('getUserStatus', user);
+console.log('Current user', currentuser);
 
 export default {
     name: 'home',
@@ -65,6 +66,9 @@ export default {
             firebase.auth().signInWithPopup(provider)
                 // eslint-disable-next-line
                 .then((result) => {
+                    // eslint-disable-next-line
+                    console.log('Result:', result);
+                    this.user = result.user.uid;
                     return result;
                 })
                 // eslint-disable-next-line
@@ -72,9 +76,33 @@ export default {
                     return error;
                 });
         },
-    },
-    mounted() {
-        firebase.auth().onAuthStateChanged(this.onUserLogin);
+        onUserSignOut() {
+            firebase.auth().signOut()
+                // eslint-disable-next-line
+                .then(() => {
+                    this.user = undefined;
+                    //eslint-disable-next-line
+                    console.log('Signed out');
+                })
+                // eslint-disable-next-line
+                .catch((error) => {
+                    //eslint-disable-next-line
+                    console.log('Sign out error', error);
+                });
+        },
+        checkUser() {
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    this.user = user;
+                    //eslint-disable-next-line
+                    console.log('Check user', user);
+                } else {
+                    this.user = undefined;
+                    //eslint-disable-next-line
+                    console.log('User not logged in');
+                }
+            });
+        },
     },
     components: {
         addEvent: AddEvent,
@@ -127,13 +155,14 @@ export default {
     overflow: hidden;
 }
 
-.bananameister__add-event {
+.bananameister__add-event,
+.bananameister__signinout {
     position: relative;
     font-size: 12px;
     display: inline-block;
     width: auto;
     float: right;
-    margin: 13px 0;
+    margin: 13px 0 13px 5px;
     background-color: #eaeaea;
     border: 1px solid #d5d5d5;
     border-radius: 5px;
