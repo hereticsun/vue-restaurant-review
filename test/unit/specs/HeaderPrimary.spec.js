07be47d810cd15f4c8e653c6474704a4c9d3c0b4
Azlan Cuttilan <azlan@bynd.com>
2017-02-21 11:42:27 +0000

    Test spec for header-primary
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

---
+++ test/unit/specs/HeaderPrimary.spec.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import HeaderPrimary from 'src/components/common/header-primary/HeaderPrimary';
+
+describe('HeaderPrimary.vue', () => {
+    it('should render correct content', () => {
+        const vm = new Vue({
+            el: document.createElement('div'),
+            render: (h) => h(HeaderPrimary),
+        });
+        expect(vm.$el.querySelector('.header-primary h1').textContent)
+            .to..be.a('string');
+        expect(vm.$el.querySelector('.header-primary h1').textContent)
+            .to.equal('Tech Lunch');
+    });
+});
