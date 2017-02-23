0f3a03aef405b3b1a64588d375ad34fb69c07402
Azlan Cuttilan <azlan@bynd.com>
2017-02-23 16:21:52 +0000

    Improve unit spec test for HeaderPrimary
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- test/unit/specs/HeaderPrimary.spec.js
+++ test/unit/specs/HeaderPrimary.spec.js
@@ -2,14 +2,22 @@ import Vue from 'vue';
 import HeaderPrimary from 'src/components/common/header-primary/HeaderPrimary';
 
 describe('HeaderPrimary.vue', () => {
-    it('should render correct content', () => {
-        const vm = new Vue({
-            el: document.createElement('div'),
-            render: (h) => h(HeaderPrimary),
+    describe('Page title', () => {
+        it('should render a string', () => {
+            const vm = new Vue({
+                el: document.createElement('div'),
+                render: (h) => h(HeaderPrimary),
+            });
+            expect(vm.$el.querySelector('.header-primary h1').textContent)
+                .to.be.a('string');
         });
-        expect(vm.$el.querySelector('.header-primary h1').textContent)
-            .to.be.a('string');
-        expect(vm.$el.querySelector('.header-primary h1').textContent)
-            .to.equal('Tech Lunch');
+        it('should render correct content', () => {
+            const vm = new Vue({
+                el: document.createElement('div'),
+                render: (h) => h(HeaderPrimary),
+            });
+            expect(vm.$el.querySelector('.header-primary h1').textContent)
+                .to.equal('Tech Lunch');
+        });
     });
 });
